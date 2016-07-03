1437f9428a2167a30e16b99de05dc062bb427e17
quwei913 <quwei913@gmail.com>
2016-07-03 20:23:20 +1000

    1st backup

--- python/Django/hello/urls.py
+++ python/Django/hello/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.hello, name='hello'),
+    url(r'^time$', views.current_datetime, name='current_datetime'),
+    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead, name='hours_ahead'),
 ]
